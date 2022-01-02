01f41065cf31bf02373e72aa2b6754dae8f9304b
AkashP27 <akashphayade27@gmail.com>
2022-01-02 17:14:43 +0530

    minor changes

--- JWT/app.js
+++ JWT/app.js
@@ -1,5 +1,30 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+const mongoSanitize = require("express-mongo-sanitize");
+
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
+
+const limiter = rateLimit({
+ max: 100,
+ windowMs: 60 * 60 * 1000,
+ message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+app.use(express.json({ limit: "10kb" }));
+
+app.use(mongoSanitize());
+
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+ next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
