8debd11083fb3b07f88f5e8f0e0006c4d76fec53
AkashP27 <akashphayade27@gmail.com>
2022-03-16 15:32:08 +0530

    added routes

--- ImageGallery/image.js
+++ ImageGallery/image.js
@@ -10,12 +10,13 @@ router.post("/", upload.single("image"), async (req, res) => {
    folder: "ImageGallery",
   });
 
-  // Create new user
+  // Create new image
   let images = new Image({
    name: req.body.name,
    cloudinary_id: result.public_id,
   });
-  // Save user
+
+  // Save image
   await images.save();
   res.json(images);
  } catch (err) {
@@ -23,59 +24,13 @@ router.post("/", upload.single("image"), async (req, res) => {
  }
 });
 
-// router.get("/", async (req, res) => {
-//  try {
-//   let user = await User.find();
-//   res.json(user);
-//  } catch (err) {
-//   console.log(err);
-//  }
-// });
-
-// router.delete("/:id", async (req, res) => {
-//  try {
-//   // Find user by id
-//   let user = await User.findById(req.params.id);
-//   // Delete image from cloudinary
-//   await cloudinary.uploader.destroy(user.cloudinary_id);
-//   // Delete user from db
-//   await user.remove();
-//   res.json(user);
-//  } catch (err) {
-//   console.log(err);
-//  }
-// });
-
-// router.put("/:id", upload.single("image"), async (req, res) => {
-//  try {
-//   let user = await User.findById(req.params.id);
-//   // Delete image from cloudinary
-//   await cloudinary.uploader.destroy(user.cloudinary_id);
-//   // Upload image to cloudinary
-//   let result;
-//   if (req.file) {
-//    result = await cloudinary.uploader.upload(req.file.path);
-//   }
-//   const data = {
-//    name: req.body.name || user.name,
-//    avatar: result?.secure_url || user.avatar,
-//    cloudinary_id: result?.public_id || user.cloudinary_id,
-//   };
-//   user = await User.findByIdAndUpdate(req.params.id, data, { new: true });
-//   res.json(user);
-//  } catch (err) {
-//   console.log(err);
-//  }
-// });
-
-// router.get("/:id", async (req, res) => {
-//  try {
-//   // Find user by id
-//   let user = await User.findById(req.params.id);
-//   res.json(user);
-//  } catch (err) {
-//   console.log(err);
-//  }
-// });
+router.get("/", async (req, res) => {
+ try {
+  let images = await Image.find();
+  res.json(images);
+ } catch (err) {
+  console.log(err);
+ }
+});
 
 module.exports = router;
