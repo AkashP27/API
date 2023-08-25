3e00c03c3dc9d98932bf9b2694b25e0851e1a3e3
AkashP27 <akashphayade27@gmail.com>
2023-08-25 16:57:20 +0530

    imageurl field added

--- ImageGallery/model/Image.js
+++ ImageGallery/model/Image.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
 const imageSchema = new mongoose.Schema({
- name: {
-  type: String,
- },
- cloudinary_id: {
-  type: String,
- },
+	name: {
+		type: String,
+	},
+	imageURL: {
+		type: String,
+		required: true,
+	},
+	cloudinary_id: {
+		type: String,
+	},
 });
 
 module.exports = mongoose.model("Image", imageSchema);
