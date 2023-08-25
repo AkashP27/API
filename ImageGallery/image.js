dd809ebd81f8e134ff8510bcc1887b3a7d3f953b
AkashP27 <akashphayade27@gmail.com>
2023-08-25 17:29:12 +0530

    delete handler

--- ImageGallery/image.js
+++ ImageGallery/image.js
@@ -1,36 +1,59 @@
 const router = require("express").Router();
-const cloudinary = require("./cloudinary");
-const upload = require("./multer");
+const cloudinary = require("./utils/cloudinary");
+const upload = require("./utils/multer");
 const Image = require("./model/Image");
 
 router.post("/", upload.single("image"), async (req, res) => {
- try {
-  // Upload image to cloudinary
-  const result = await cloudinary.uploader.upload(req.file.path, {
-   folder: "ImageGallery",
-  });
+	// console.log(req.file.path);
+	try {
+		// Upload image to cloudinary
+		const result = await cloudinary.uploader.upload(req.file.path, {
+			folder: "ImageGallery",
+		});
 
-  // Create new image
-  let images = new Image({
-   name: req.body.name,
-   cloudinary_id: result.public_id,
-  });
+		// Create new image
+		let newImage = new Image({
+			name: req.body.name,
+			imageURL: result.secure_url,
+			cloudinary_id: result.public_id,
+		});
 
-  // Save image
-  await images.save();
-  res.json(images);
- } catch (err) {
-  console.log(err);
- }
+		// Save image
+		await newImage.save();
+
+		res.status(201).json({
+			status: "success",
+			newImage,
+		});
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 router.get("/", async (req, res) => {
- try {
-  let images = await Image.find();
-  res.json(images);
- } catch (err) {
-  console.log(err);
- }
+	try {
+		let images = await Image.find();
+		res.status(200).json({
+			status: "success",
+			images,
+		});
+	} catch (err) {
+		console.log(err);
+	}
+});
+
+router.delete("/:id", async (req, res) => {
+	try {
+		// console.log(req.params.id);
+		const image = await Image.findById(req.params.id);
+		await cloudinary.uploader.destroy(image.cloudinary_id);
+		await image.delete();
+		res.status(204).json({
+			status: "Image deleted",
+		});
+	} catch (err) {
+		console.log(err);
+	}
 });
 
 module.exports = router;
--- ImageGallery/server.js
+++ ImageGallery/server.js
@@ -7,20 +7,20 @@ const cors = require("cors");
 dotenv.config({ path: "config.env" });
 
 mongoose
- .connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: true,
- })
- .then(console.log("connection successful"))
- .catch((err) => console.log(err));
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		// useCreateIndex: true,
+		// useFindAndModify: true,
+	})
+	.then(console.log("connection successful"))
+	.catch((err) => console.log(err));
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // Route
-app.use("/uploadImage", require("./image"));
+app.use("/image", require("./image"));
 
 app.listen(5000, () => console.log("Server is running on 5000"));
