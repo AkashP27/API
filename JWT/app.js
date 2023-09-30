9db1047c80a916cc2814b623e2e9569107443559
AkashP27 <akashphayade27@gmail.com>
2023-09-30 19:50:49 +0530

    refactoring code

--- JWT/app.js
+++ JWT/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const mongoSanitize = require("express-mongo-sanitize");
 
@@ -8,10 +9,14 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
 const limiter = rateLimit({
- max: 100,
- windowMs: 60 * 60 * 1000,
- message: "Too many requests from this IP, please try again in an hour!",
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: "Too many requests from this IP, please try again in an hour!",
 });
 app.use("/api", limiter);
 
@@ -22,7 +27,7 @@ app.use(mongoSanitize());
 app.use("/api/v1/users", userRouter);
 
 app.all("*", (req, res, next) => {
- next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+	next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
 app.use(globalErrorHandler);
